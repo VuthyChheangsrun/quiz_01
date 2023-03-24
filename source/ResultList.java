6d12de9df4e1acda7c1e4a5e620d1944a4b54381
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2023-03-24 15:49:33 +0700

    fixed

--- source/ResultList.java
+++ source/ResultList.java
@@ -1,18 +1,18 @@
 package source;
 
 public class ResultList {
-    void Display(){
+    public static void main(String[] args) {
         System.out.println(
-            """
-            Test Result:
-                Category                Score
-                WPM                     40
-                acc                     88%
-                raw                     49
-                characters              99/7/3/0
-                consistency             69%
-                time                    31s
-            """
-        );
-    }
+                """
+                Test Result:
+                    Category                Score
+                    WPM                     40
+                    acc                     88%
+                    raw                     49
+                    characters              99/7/3/0
+                    consistency             69%
+                    time                    31s
+                """
+            );
+        }        
 }
